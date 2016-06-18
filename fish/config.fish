73d9c901c6556d692aa82448acaeff55d2bdab4f
Andrew Klaus <andrewklaus@gmail.com>
2016-06-17 23:14:30 -0600

    Adding fish aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,17 @@
+set --erase fish_greeting
+
+
+# Aliases
+balias gws "git status --short"
+balias gc "git commit"
+balias gca "git commit -a"
+balias gco "git checkout"
+balias ga "git add"
+balias gb "git branch"
+balias gm "git merge"
+balias gps "git push"
+balias gpl "git pull"
 alias rm="rm -i"
 alias vi="nvim"
-alias vimdiff="nvim -d"
+alias vim="nvim"
+alias vimdiff="vim -d"
