ebb1d8bc6e098668def879dbd565975783beab6e
Andrew Klaus <andrewklaus@gmail.com>
2016-07-19 21:10:24 -0600

    Adding laptop configs

---
+++ install_laptop.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+DIR="$(cd "$(dirname "$0")" && pwd)"
+
+ln -s "$DIR/Xresources" ~/.Xresources
+ln -s "$DIR/Xmodmap" ~/.Xmodmap
+ln -s "$DIR/tmux.conf" ~/.tmux.conf
+ln -s "$DIR/i3-laptop/config" ~/.config/i3/config
+ln -s "$DIR/i3-laptop/i3status.conf" ~/.i3status.conf
+ln -s "$DIR/.config/nvim" ~/.config/nvim
+ln -s "$DIR/.config/fish" ~/.config/fish
