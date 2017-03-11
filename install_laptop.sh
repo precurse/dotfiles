40baf661e92e70ea61e3d4bf3b656941552702af
Andrew Klaus <andrewklaus@gmail.com>
2017-03-11 12:04:30 -0700

    Adding bashrc

--- install_laptop.sh
+++ install_laptop.sh
@@ -4,6 +4,7 @@ DIR="$(cd "$(dirname "$0")" && pwd)"
 ln -s "$DIR/Xresources" ~/.Xresources
 ln -s "$DIR/Xmodmap" ~/.Xmodmap
 ln -s "$DIR/tmux.conf" ~/.tmux.conf
+ln -s "$DIR/bashrc" ~/.bashrc
 ln -s "$DIR/i3-laptop/config" ~/.config/i3/config
 ln -s "$DIR/i3-laptop/i3status.conf" ~/.i3status.conf
 ln -s "$DIR/.config/nvim" ~/.config/nvim
