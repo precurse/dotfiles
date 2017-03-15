ae367060f0ceb639081f8284e7438f103c9d8c75
Andrew Klaus <andrewklaus@gmail.com>
2017-03-14 23:17:46 -0600

    Renaming install file

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,8 +2,6 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 
 ln -s "$DIR/gitconfig" ~/.gitconfig
-ln -s "$DIR/Xresources" ~/.Xresources
-ln -s "$DIR/Xmodmap" ~/.Xmodmap
 ln -s "$DIR/tmux.conf" ~/.tmux.conf
 ln -s "$DIR/.config/nvim" ~/.config/nvim
 
@@ -14,6 +12,10 @@ ln -s "$DIR/bash_profile" ~/.bash_profile
 ln -s "$DIR/zshrc" ~/.zshrc
 ln -s "$DIR/.config/fish" ~/.config/fish
 ln -s "$DIR/logout" ~/.logout
+
+# X11
+ln -s "$DIR/Xresources" ~/.Xresources
+ln -s "$DIR/Xmodmap" ~/.Xmodmap
 
 # i3
 ln -s "$DIR/i3-laptop/config" ~/.config/i3/config
