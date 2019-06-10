c5d1d763ce08d186269cb8f052c414e2fd692851
Andrew Klaus <andrew@aklaus.ca>
2019-06-10 11:49:28 -0600

    Fixing install.sh logic

--- install.sh
+++ install.sh
@@ -8,25 +8,38 @@ if ! command -v stow >/dev/null; then
     if [ -f /etc/os-release ]; then
         . /etc/os-release
 
-        if [ $ID == "fedora" ]; then
+        if [ "$ID" = "fedora" ]; then
             sudo dnf install -y stow
         fi
 
-        if [ $ID == "debian" ]; then
+        if [ "$ID" = "debian" ] || [ "$ID" = "ubuntu" ]; then
             sudo apt install -y stow
         fi
     else
-        echo "Please install GNU stow manually"
+        # Use uname instead
+        case "$(uname -s)" in
+          OpenBSD)
+            doas pkg_add stow
+            ;;
+
+          Darwin)
+            brew update
+            brew install stow
+            ;;
+          *)
+            echo "Please install GNU stow manually"
+            ;;
+        esac
     fi
 fi
 
 
 # Install all
 stow git
+stow shell
 #stow emacs
 #stow nvim
 #stow i3
-#stow shell
 #stow ssh
 #stow tmux
 #stow x11
